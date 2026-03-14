708c283d2ca8d9f1fdc0ea5c18e1dc54fd433b85
Geguuuul <tjdud020512s@naver.com>
2026-03-14 12:30:05 +0900

    폴더 삭제 경로에 / 누락 추가

--- src/vault.cpp
+++ src/vault.cpp
@@ -127,7 +127,7 @@ void Vault::execute(Command& cmd) {
             throw std::runtime_error("init requires <vault_name> [folder_path]");
         }
         std::string vault_name = normalize_vault_name(cmd.positional[0]);
-        std::filesystem::path local_vault_dir = getHomeDirectory() + ".gitvault/" + vault_name;
+        std::filesystem::path local_vault_dir = getHomeDirectory() + "/.gitvault/" + vault_name;
         if (std::filesystem::exists(local_vault_dir)) {
             std::error_code ec;
             std::filesystem::remove_all(local_vault_dir, ec);
